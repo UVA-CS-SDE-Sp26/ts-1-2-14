df40838d7f19c7592e35e538a618c7505d80ea45
chgaobt <chloejgao@gmail.com>
2026-02-12 14:06:05 -0500

    FileHandler testing changes

--- src/tests/FileHandlerTest.java
+++ src/tests/FileHandlerTest.java
@@ -7,20 +7,5 @@ import static org.mockito.Mockito.when;
 
 
 class FileHandlerTest {
-
-    @org.junit.jupiter.api.Test
-    void getFolderPath() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getFileContents() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void setFolderPath() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testToString() {
-    }
+    
 }
