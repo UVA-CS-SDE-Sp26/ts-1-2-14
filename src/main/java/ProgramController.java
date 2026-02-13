cf20dd1a8c7b92bdd1d6fc60fec744f2126fbcf7
ldocs17 <dcosta.luc@gmail.com>
2026-02-12 20:49:11 -0500

    Pushing so I can merge

--- src/main/java/ProgramController.java
+++ src/main/java/ProgramController.java
@@ -9,6 +9,7 @@ public class ProgramController {
 
     public void cmd_line_util(){
         int arg_len = args.length;
+        int file_number;
         switch(arg_len){
             case(0):
                 //Jenny this is your part
@@ -16,38 +17,26 @@ public class ProgramController {
 
                 break;
             case(1):
-                try {
-                    //attempt conversion to int
-                    int file_number = Integer.parseInt(args[1]);
-                    //Need to check to see if int is greater than number of files
+                //attempt conversion to int
+                file_number = Integer.parseInt(args[0]);
+                //Need to check to see if int is greater than number of files
 
-                    //Jenny this is your part
-                    //print file with default cipher
+                //Jenny this is your part
+                //print file with default cipher
 
-                    break;
-                } catch (NumberFormatException e) {
-                    //handle catch
-                    System.out.println("Error: Bad input detected input is either not an int or not a file option");
-                }
                 break;
             case(2):
                 //print file using the given cipher
-                try {
-                    //attempt conversion to int
-                    int file_number = Integer.parseInt(args[1]);
-                    //Need to check to see if int is greater than number of files
+                //attempt conversion to int
+                file_number = Integer.parseInt(args[0]);
+                //Need to check to see if int is greater than number of files
 
-                    //Jenny this is your part
+                // Jenny this is your part
 
-                    break;
-                } catch (NumberFormatException e) {
-                    //handle catch
-                    System.out.println("Error: Bad input detected input is either not an int or not a file option");
-                    break;
-                }
+                break;
             default:
-                System.out.println("Error: too many arguments provided.");
-                System.out.println("Please provide 0,1 or 2 arguments");
+                System.err.println("Error: too many arguments provided.");
+                System.err.println("Please provide 0,1 or 2 arguments");
     }
 
     }
